6e943b15a932ef02ca7ae7cf8fc59d4339528a36
Mandeep Malakar <tagdacoder@gmail.com>
2025-09-14 12:24:26 +0530

    Project Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-zinc-900 w-full h-screen'>App</div>
+    <div className='bg-zinc-900 w-full h-screen text-[2rem] flex justify-center items-center'>
+      Mr.Bike Project
+    </div>
   )
 }
 
