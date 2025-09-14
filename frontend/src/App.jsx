1d39a96827f40a4b2914c1014aafd9126d7c7b67
Mandeep Malakar <tagdacoder@gmail.com>
2025-09-14 12:34:53 +0530

    Updating Some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-zinc-900 w-full h-screen text-[2rem] flex justify-center items-center'>
+    <div className='text-yellow-300 bg-zinc-900 w-full h-screen text-[4rem] font-semibold selection:text-blue-300 flex justify-center items-center'>
       Mr.Bike Project
     </div>
   )
